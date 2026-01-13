c9042376678a08737def9e6ce87c03eda7078251
naybriones202 <nbriones4760@utm.edu.ec>
2026-01-13 00:20:01 -0500

    Create index.js

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const __dirname = path.resolve();
+
+// Servir frontend
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/index.html"));
+});
+
+app.listen(10000, () => {
+  console.log("Servidor activo en puerto 10000");
+});
+
